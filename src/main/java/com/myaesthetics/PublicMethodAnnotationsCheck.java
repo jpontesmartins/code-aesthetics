071dd677693866e4def157b27fb8631e4b9137e4
João Pedro Pontes Martins <jpontesmartins@gmail.com>
2024-07-14 10:30:50 -0300

    Refatoracao PublicMethodAnnotations

--- src/main/java/com/myaesthetics/PublicMethodAnnotationsCheck.java
+++ src/main/java/com/myaesthetics/PublicMethodAnnotationsCheck.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 public class PublicMethodAnnotationsCheck extends BaseCheck {
 
+    private static final String METHOD_MISSING_ANNOTATION = "method.missing.annotation";
     private Set<String> classSuffixes = new HashSet<>();
     private Set<String> requiredAnnotations = new HashSet<>();
 
@@ -17,35 +18,33 @@ public class PublicMethodAnnotationsCheck extends BaseCheck {
 
         if (classSuffixes.stream().anyMatch(className::endsWith)) {
             DetailAST nodeCodeBlock = node.findFirstToken(TokenTypes.OBJBLOCK);
-                for (DetailAST child = nodeCodeBlock.getFirstChild(); child != null; child = child.getNextSibling()) {
-                    if (child.getType() == TokenTypes.METHOD_DEF) {
-                        Set<String> foundAnnotations = new HashSet<>();
-
-                        if (checkIfPublicModifier(child)) {
-                            searchForAnnotations(child, foundAnnotations);
-                            checkIfAnnotationIsPresent(child, foundAnnotations);
-                        }
+            for (DetailAST child = nodeCodeBlock.getFirstChild(); child != null; child = child.getNextSibling()) {
+                if (child.getType() == TokenTypes.METHOD_DEF) {
+                    Set<String> foundAnnotations = new HashSet<>();
+                    if (isPublicModifier(child)) {
+                        searchForAnnotations(child, foundAnnotations);
+                        checkIfAnnotationIsPresent(child, foundAnnotations);
                     }
                 }
+            }
         }
 
     }
 
-    private void checkIfAnnotationIsPresent(DetailAST child, Set<String> foundAnnotations) {
+    private void checkIfAnnotationIsPresent(DetailAST node, Set<String> foundAnnotations) {
         for (String requiredAnnotation : requiredAnnotations) {
             if (!foundAnnotations.contains(requiredAnnotation)) {
-                log(
-                    child.getLineNo(), 
-                    "method.missing.annotation", 
-                    child.findFirstToken(TokenTypes.IDENT).getText(), requiredAnnotation
-                );
+                log(node.getLineNo(),
+                    METHOD_MISSING_ANNOTATION,
+                    node.findFirstToken(TokenTypes.IDENT).getText(), requiredAnnotation);
             }
         }
     }
 
-    private void searchForAnnotations(DetailAST child, Set<String> foundAnnotations) {
-        DetailAST nodeModifier = child.findFirstToken(TokenTypes.MODIFIERS);
-        for (DetailAST annotation = nodeModifier.getFirstChild(); annotation != null; annotation = annotation.getNextSibling()) {
+    private void searchForAnnotations(DetailAST node, Set<String> foundAnnotations) {
+        DetailAST nodeModifier = node.findFirstToken(TokenTypes.MODIFIERS);
+        for (DetailAST annotation = nodeModifier.getFirstChild(); annotation != null; annotation = annotation
+                .getNextSibling()) {
             if (annotation.getType() == TokenTypes.ANNOTATION) {
                 DetailAST annotationIdent = annotation.findFirstToken(TokenTypes.IDENT);
                 foundAnnotations.add(annotationIdent.getText());
@@ -53,7 +52,7 @@ public class PublicMethodAnnotationsCheck extends BaseCheck {
         }
     }
 
-    private boolean checkIfPublicModifier(DetailAST node) {
+    private boolean isPublicModifier(DetailAST node) {
         DetailAST nodeModifier = node.findFirstToken(TokenTypes.MODIFIERS);
         for (DetailAST modifier = nodeModifier.getFirstChild(); modifier != null; modifier = modifier.getNextSibling()) {
             if (modifier.getType() == TokenTypes.LITERAL_PUBLIC) {
@@ -73,7 +72,7 @@ public class PublicMethodAnnotationsCheck extends BaseCheck {
 
     @Override
     public int[] getDefaultTokens() {
-        return new int[]{TokenTypes.CLASS_DEF};
+        return new int[] { TokenTypes.CLASS_DEF };
     }
 
 }
